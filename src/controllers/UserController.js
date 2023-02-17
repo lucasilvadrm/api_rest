0f2119d5288159e2044402adb4a98e48dd1a0be7
Lucas Silva <lucassilva.uern@gmail.com>
2023-02-17 13:15:32 -0300

    feat: CRUD user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,12 +1,90 @@
 import User from '../models/User';
 
 class UserController {
+  // post -> cria um novo usuário
   async store(req, res) {
     try {
       const novoUser = await User.create(req.body);
-      res.json(novoUser);
+      return res.json(novoUser);
     } catch (error) {
-      res.status(400).json({
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // index - GET -> lista todos os usuários
+  async index(req, res) {
+    try {
+      const users = await User.findAll();
+      return res.json(users);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Show - GET -> mostra um usuário
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      const user = await User.findByPk(id);
+      return res.json(user);
+    } catch (error) {
+      return res.json(null);
+    }
+  }
+
+  // Update - PATCH/PUT -> atualiza um usuário
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['id não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      const novosDados = await user.update(req.body);
+      return res.json(novosDados);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  // Delete - DELETE -> apaga um usuário
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+
+      if (!id) {
+        return res.status(400).json({
+          errors: ['id não enviado.'],
+        });
+      }
+
+      const user = await User.findByPk(id);
+
+      if (!user) {
+        return res.status(400).json({
+          errors: ['Usuário não existe.'],
+        });
+      }
+
+      await user.destroy();
+      return res.json(user);
+    } catch (error) {
+      return res.status(400).json({
         errors: error.errors.map((err) => err.message),
       });
     }
